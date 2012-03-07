9f70b7a42c8e2fe35882ac9eb0bfc8b9aef13244
JB Van Puyvelde <jbvanpuyvelde@gmail.com>
2012-03-07 19:45:23 +0100

    Improve speed by not moving the same piece 2 twice in a row

--- ums.h
+++ ums.h
@@ -5,6 +5,7 @@ struct node {
   struct node *prev;
   struct node *next;
   char move[6][6];
+  unsigned short piece_moved_i, piece_moved_j;
 };
 
 // FIFO
@@ -20,6 +21,6 @@ unsigned int fifo_size = 0;  // the total number of tested moves
 unsigned int prev_depth = 0;
 
 void next_moves();
-void add(char move[][6]);
+void add(char move[][6], unsigned short i, unsigned short j);
 int equals(char a[][6], char b[][6]);
 void printm(char move[][6]);
